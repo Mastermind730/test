470869e4cfd070c574b0d0e77bf864050e71b172
Mastermind730 <sourav.97032@gmail.com>
2024-10-10 11:37:48 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 
 const nextConfig = {
   output: 'export',
-  // basePath: '/your-subdirectory', // Add if deploying to a subdirectory
   images: {
     domains: ['i.postimg.cc', 'pcet.org.in', 'via.placeholder.com'],
     unoptimized: true,
